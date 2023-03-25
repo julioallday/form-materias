cb15ab097afb173afd76b4d64d40f76485006741
julioallday <naruto481@gmail.com>
2023-03-25 22:54:53 +0000

    feat(template): cria modal e form-materias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormMateriasComponent } from './components/form-materias/form-materias.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormMateriasComponent,
+    ModalComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form-materias/form-materias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-materias',
+  templateUrl: './form-materias.component.html',
+  styleUrls: ['./form-materias.component.css']
+})
+export class FormMateriasComponent {
+
+}
