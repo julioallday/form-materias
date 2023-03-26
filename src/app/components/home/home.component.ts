5e9ec0d56785dae1d72d0e7e1aa8cf18cdbb33f2
julioallday <naruto481@gmail.com>
2023-03-26 15:43:19 +0000

    feat(component): cria output home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   getInputText(event: any) {
-    console.log(event);
+    console.log(event.target.value);
     
   }
 }
