147d1692b6ce1c9f1d12497629159647a6745a2c
julioallday <naruto481@gmail.com>
2023-03-27 19:23:36 -0300

    fix(style):adiciona parallax cores e fundo.

--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -3,9 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'input-form',
   templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.css']
+  styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent {
-  @Input() etiqueta = "";
-  @Input() tipo = "";
+  @Input() etiqueta = '';
+  @Input() tipo = '';
+  @Input() classes = '';
+  @Input() mensagemEspacoReservado = '';
 }
